5984c7871d8420a7ce962604e9c845751529a1a5
kennby <153956421+kennby@users.noreply.github.com>
2023-12-30 23:07:23 -0500

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -8,10 +8,10 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 • *Link :* https://paypal.me/fg98f
 
 ▢ *Mercado Pago Arg*
-• *Alias :* fgmods
+• *Alias :* onyx
 
 ▢ *Naranja X Arg*
-• *Alias :* fgmods2
+• *Alias :* onyx
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m, null, rpyp)
