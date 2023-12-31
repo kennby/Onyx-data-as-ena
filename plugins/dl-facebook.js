6dadfdb09c97f08cc1ca49ca0e9ece5a42b0ef9f
kennby <153956421+kennby@users.noreply.github.com>
2023-12-31 14:59:03 -0500

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -8,7 +8,7 @@ if (!args[0]) throw `✳️ ${mssg.noLink('Facebook')}\n\n📌 ${mssg.example} :
   try {
     let result = await fg.fbdl(args[0]);
     let tex = `
-┌─⊷ *FBDL*
+┌─⊷ *FB ONYX*
 ▢ *${mssg.title}:* ${result.title}
 └───────────`;
     conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m);
